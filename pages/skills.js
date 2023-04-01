31560208f038dae39fab7eb77048954d539b24b8
Hideki-Kawachi <hideki.kawachi17@gmail.com>
2023-04-02 02:08:08 +0800

    Updated skills

--- pages/skills.js
+++ pages/skills.js
@@ -18,27 +18,32 @@ function Skills() {
 							<strong style={{ color: "#61DAFB" }}> React </strong>
 							and
 							<strong style={{ color: "#E4E4E4" }}> Next.js </strong>
-							while using <strong style={{ color: "#8CC84B" }}>Express </strong>
-							along with
-							<strong style={{ color: "#14A506" }}> mongoDB</strong> or{" "}
-							<strong style={{ color: "#E38D1A" }}>mySQL</strong> for the
-							backend. I&rsquo;m comfortable with and have prior experience in
-							working with teams adopting{" "}
-							<strong style={{ color: "#FF76AF" }}>agile</strong> methodologies
-							such as scrum.
+							while using <strong style={{ color: "#8CC84B" }}>
+								Express
+							</strong>{" "}
+							and Javascript along with databases like
+							<strong style={{ color: "#14A506" }}> mongoDB</strong> ,
+							<strong style={{ color: "#E38D1A" }}> mySQL</strong>, and{" "}
+							<strong style={{ color: "#FEBA00" }}>Firebase</strong> for the
+							backend. I also have experience developing mobile applications for
+							Android using <strong style={{ color: "#8949FB" }}>Kotlin</strong>{" "}
+							and <strong style={{ color: "#EC2025" }}>Java</strong>. I&rsquo;m
+							comfortable with and have prior experience in working with teams
+							adopting <strong style={{ color: "#FF76AF" }}>agile</strong>{" "}
+							methodologies such as scrum.
 							<br></br>
 							<br></br>
-							I&rsquo;ve also been studying and trying out new technologies such
-							as <b>Framer Motion</b>, <b>Three.js</b>, and <b>React Spring</b>{" "}
-							for website animations. Along with this, I have experience in
-							using <b>Figma </b>
+							Recently, I&rsquo;ve also been studying and trying out new
+							technologies such as <b>Framer Motion</b>, <b>Three.js</b>, and{" "}
+							<b>React Spring</b> for website animations. Along with this, I
+							have experience in using <b>Figma </b>
 							for making prototypes and <b>Lucidchart</b> for creating BPMN 2.0
 							diagrams.
 						</p>
 					</div>
 					<div className="skills-list-main-container">
 						<div className="skills-list-container">
-							<h3>FRONTEND</h3>
+							<h3>FRAMEWORKS & LANGUAGES</h3>
 							<SkillsCard
 								level={"90%"}
 								color={"#F0DB4F"}
@@ -57,32 +62,44 @@ function Skills() {
 								caption={"Next.js"}
 								key={3}
 							></SkillsCard>
-						</div>
-						<div className="skills-list-container">
-							<h3>BACKEND</h3>
 							<SkillsCard
-								level={"90%"}
-								color={"#F0DB4F"}
-								caption={"Javascript"}
+								level={"70%"}
+								color={"#8949FB"}
+								caption={"Kotlin"}
 								key={4}
 							></SkillsCard>
+							<SkillsCard
+								level={"70%"}
+								color={"#EC2025"}
+								caption={"Java"}
+								key={5}
+							></SkillsCard>
+						</div>
+						<div className="skills-list-container">
+							<h3>TOOLS</h3>
 							<SkillsCard
 								level={"75%"}
 								color={"#8CC84B"}
 								caption={"Express"}
-								key={5}
+								key={6}
 							></SkillsCard>
 							<SkillsCard
 								level={"85%"}
 								color={"#14A506"}
 								caption={"MongoDB"}
-								key={6}
+								key={7}
 							></SkillsCard>
 							<SkillsCard
 								level={"70%"}
 								color={"#E38D1A"}
 								caption={"MySQL"}
-								key={7}
+								key={8}
+							></SkillsCard>
+							<SkillsCard
+								level={"75%"}
+								color={"#FEBA00"}
+								caption={"Firebase"}
+								key={9}
 							></SkillsCard>
 						</div>
 					</div>
